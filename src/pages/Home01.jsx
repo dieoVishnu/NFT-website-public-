7583a469f68e346711880b4d0fb9533e3e769d3d
vishnu <vishnu@dieofficial.com>
2022-04-05 16:05:12 +0400

    width of header inscresed to 40% and --bg-slider gradinet color changed

--- src/pages/Home01.jsx
+++ src/pages/Home01.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Header from '../components/header/Header';
-import Slider01 from '../components/slider/Slider01';
 import dataSlider from '../assets/fake-data/dataSlider';
-import LiveAution from '../components/layouts/home/LiveAution';
 import dataLiveAution from '../assets/fake-data/dataLiveAution'
-import LatestCollections from '../components/layouts/home/LatestCollections';
-import dataCollections from '../assets/fake-data/dataCollections'
-import BestSeller from '../components/layouts/home/BestSeller';
-import dataBestSeller from '../assets/fake-data/dataBestSeller'
-import TrendingCollections from '../components/layouts/home/TrendingCollections';
-import dataTrendingCollections from '../assets/fake-data/dataTrendingCollections'
-import Category from '../components/layouts/home/Category';
 import dataCategory from '../assets/fake-data/dataCategory'
 import Footer from '../components/footer/Footer';
-import { Newsletters } from '../components/layouts/home/Newsletters';
 import Slider03 from '../components/slider/Slider03';
 import Gallery from '../components/layouts/home/Gallery';
 import Rareitems from '../components/layouts/home/Rareitems';
 import { PurchaseOpensea } from '../components/layouts/home/PurchaseOpensea';
+import Slider01 from '../components/slider/Slider01';
+// import LiveAution from '../components/layouts/home/LiveAution';
+// import LatestCollections from '../components/layouts/home/LatestCollections';
+// import dataCollections from '../assets/fake-data/dataCollections'
+// import BestSeller from '../components/layouts/home/BestSeller';
+// import dataBestSeller from '../assets/fake-data/dataBestSeller'
+// import TrendingCollections from '../components/layouts/home/TrendingCollections';
+// import dataTrendingCollections from '../assets/fake-data/dataTrendingCollections'
+// import Category from '../components/layouts/home/Category';
+// import { Newsletters } from '../components/layouts/home/Newsletters';
 
 const Home01 = () => {
   return <div className='home-1'>
